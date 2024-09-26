ab7034105993cf7665b6747015a5a48146797233
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-26 20:08:30 +0200

    Add rainbow feature

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const gridContainer = document.querySelector("#grid-container");
 const resizeBtn = document.querySelector("#resize-btn");
+const body = document.querySelector("body");
 
 const maxWidth = +window.getComputedStyle(gridContainer).width.slice(0, -2);
 const maxHeight = +window.getComputedStyle(gridContainer).height.slice(0, -2);
@@ -30,18 +31,11 @@ function userInputIsValid(userInput){
     return false;
 }
 
-function generateGridElement(elementXSize, elementYSize){
-    let element = document.createElement("div");
-    element.classList.add("box");
-    element.style.width = elementXSize + "px";
-    element.style.height = elementYSize + "px";
-    return element;
-}
-
-function calculateSizeOfElements(width, height){
-    let xSize = ((maxWidth-((width-1)*gapSize))/width);
-    let ySize = ((maxHeight-((height-1)*gapSize))/height);
-    return [xSize, ySize];
+function deleteGrid(){
+    const allGridElements = document.querySelectorAll("#grid-container > div.box");
+    for(let gridElement of allGridElements){
+        gridContainer.removeChild(gridElement);
+    }
 }
 
 function generateGrid(width, height){
@@ -52,9 +46,42 @@ function generateGrid(width, height){
     }
 }
 
-function deleteGrid(){
-    const allGridElements = document.querySelectorAll("#grid-container > div.box");
-    for(let gridElement of allGridElements){
-        gridContainer.removeChild(gridElement);
-    }
+function calculateSizeOfElements(width, height){
+    let xSize = ((maxWidth-((width-1)*gapSize))/width);
+    let ySize = ((maxHeight-((height-1)*gapSize))/height);
+    return [xSize, ySize];
+}
+
+function generateGridElement(elementXSize, elementYSize){
+    let element = document.createElement("div");
+    element.classList.add("box");
+    element.style.width = elementXSize + "px";
+    element.style.height = elementYSize + "px";
+    element.addEventListener("mouseover", setRandomColorToElement);
+    element.addEventListener("mouseout", restoreBackgroundColor)
+    return element;
+}
+
+function setRandomColorToElement(event){
+    event.target.style.backgroundColor = getRandomColor();
+}
+
+function restoreBackgroundColor(event){
+    event.target.style.backgroundColor = body.style.backgroundColor;
+}
+
+function getRandomColor(){
+    let color = "#";
+    color += getRandomColorHexValue();
+    color += getRandomColorHexValue();
+    color += getRandomColorHexValue();
+    return color;
+}
+
+function getRandomColorHexValue(){
+    return getRandomNumber(256).toString(16);
+}
+
+function getRandomNumber(max){
+    return Math.floor(Math.random()*max);
 }
